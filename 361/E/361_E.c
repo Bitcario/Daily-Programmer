f6121ce766d822c22955b118aa8c5b339023978a
Bitcario <Bitcario@users.noreply.github.com>
2018-05-19 18:45:20 -0400

    361_E -- Initial commit

---
+++ 361/E/361_E.c
@@ -0,0 +1,43 @@
+/*
+ * Athr: https://github.com/Bitcario
+ * Nmbr: 361
+ * Diff: Easy
+ * Prog: Tally 
+ * Desc: https://github.com/Bitcario/Daily-Programmer/wiki/361_E 
+ * Date: 05.19.2018
+ * Note: None.
+ */
+
+// Header Files
+#include <stdio.h>
+#include <stdlib.h> 
+// Directives
+
+// Global Variables
+const char * const prog;
+// Data Structures
+
+// Function Prototypes
+
+// Macros
+#define BUF_SIZE 256
+// main()
+int 
+main(int argc, char** argv)
+{
+  // Delcarations
+  char input[BUF_SIZE};
+
+  // Assignments
+  
+  // Input
+  
+  // Process
+  
+  // Output
+
+  // Cleanup
+
+  // Fin
+  return EXIT_SUCCESS;
+}
