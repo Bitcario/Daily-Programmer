fdaedcf3f701ba71ae434355369f23f40e3715e9
Bitcario <Bitcario@users.noreply.github.com>
2020-11-30 22:20:44 -0500

    383_E: Initial commit

---
+++ 383/E/383_E.c
@@ -0,0 +1,29 @@
+/*
+  Athr: Bitcario
+  Prog: Necklace Matching 
+  Desc: See Wiki entry
+  Date: 11.30.2020
+  Note: none
+*/
+
+// Header Files
+#include <stdio.h>
+#include <stdlib.h>
+// User-defined Header Files
+
+// Directives
+
+// Global Variables
+
+// Data Structures
+
+// Function Prototypes
+
+// Macros
+
+// main()
+int main(int argc, char** argv)
+{
+
+  return EXIT_SUCCESS;
+}
